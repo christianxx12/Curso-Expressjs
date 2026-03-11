14699f82ca4ae9f34c82cb8d33beba96703d1096
Christian Rafael Guado Geronimo <crguado@gmail.com>
2026-03-10 22:07:05 -0500

    Uso de dotenv para que al hacer un cambio a las variables de entorno .env tome los nuevos cambios

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// En las versiones modernas de Node.js ya no es necesario instalar dotenv
+// si no usar el siguiente comando: node --env-file .env --watch app.js
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -5,12 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>Curso Express.js V2</h1>
+    <h1>Curso Express.js V3</h1>
     <p>Esto es una aplicación node.js con express.js</p>
     <p>Corre en el puerto ${PORT}</p>
 `);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Servidor: http://localhost:${PORT}`);
 });
