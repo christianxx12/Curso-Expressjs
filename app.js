2f97ba1c36b1c1846eebc69d480f8da112f17dd5
Christian Rafael Guado Geronimo <crguado@gmail.com>
2026-03-15 00:09:35 -0500

    Configurando body-parser y ejemplo basico de Parámetros de ruta y Parámetros de consulta

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 // si no usar el siguiente comando: node --env-file .env --watch app.js
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +16,22 @@ app.get("/", (req, res) => {
     <p>Esto es una aplicación node.js con express.js</p>
     <p>Corre en el puerto ${PORT}</p>
 `);
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(`Mostrar información del usuario con ID: ${userId}`);
+});
+
+app.get("/search", (req, res) => {
+  const terms = req.query.termino || "No especificado";
+  const category = req.query.categoria || "Todas";
+
+  res.send(`
+    <h2>Resultados de búsqueda:</h2>
+    <p>Término: ${terms}</p>
+    <p>Categoría: ${category}</p>
+  `);
 });
 
 app.listen(PORT, () => {
